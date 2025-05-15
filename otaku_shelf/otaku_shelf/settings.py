7738b9b05e3bb9694175c704ac9db0d3926c44d1
Olexandra Karbovska <olexandra.karbovska@gmail.com>
2025-05-15 19:50:14 +0300

    security issues

--- otaku_shelf/otaku_shelf/settings.py
+++ otaku_shelf/otaku_shelf/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v3fditqjnvi=rx_kbwoh$bv*(-^@d-y)vj%asl=4vud_95(cy#'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
